c59bd7829f003e1e1c9c04ad885755e1ca97189c
Raheel Ahmed <raheel25031@gmail.com>
2026-06-05 22:41:59 +0500

    Add user data to postgre sql of

--- src/components/global/bread-crumbs/automations/index.tsx
+++ src/components/global/bread-crumbs/automations/index.tsx
@@ -1,21 +1,41 @@
 import { ChevronRight, PencilLineIcon } from 'lucide-react'
 import React from 'react'
+import ActivateAutomationButton from '../../active-automation-button'
 
-type Props = {}
+type Props = {
+  id: string
+}
 
-const AutomationsBreadCrumb = (props: Props) => {
-    //WIP: get the automation data
+const AutomationsBreadCrumb = ({id}: Props) => {
+    //WIP: fetch some automation data
+    // User mutation stuff to update the autmation
   return (
-    <div className='rounded-full w-full p-5 bg-[#1818B1A] flex justify-between items-center'>
-        <div className='flex items-center gap-x-3'>
-            <p className='text-[#9B9CA0]'>Automations</p>
-            <ChevronRight color='#9B9CA0' />
-            <span className='flex gap-x-3 items-center'>
+    <div className='rounded-full w-full p-5 bg-[#1818B1A] flex items-center'>
+        <div className='flex items-center gap-x-3 min-w-0'>
+            <p className='text-[#9B9CA0] truncate'>Automations</p>
+            <ChevronRight className='flex-shrink-0' color='#9B9CA0' />
+            <span className='flex gap-x-3 items-center min-w-0'>
                 {/*WIP: Show Editing data */}
-                <p className='text-[#9B9CA0]'>This is the automation title</p>
-                {/* <PencilLineIcon /> */}
+                <p className='text-[#516fe8] truncate'>This is the automation title</p>
+                <span className='cursor-pointer hover:opacity-75 duration-100 transition flex-shrink-0 m-r-4'>
+                  <PencilLineIcon size={14}/>
+                </span>
             </span>
         </div>
+        <div className='flex items-center gap-x-5 ml-auto'>
+          <p className='hidden md:block truncate text-text-secondary/60 text-sm min-w-0'>
+            All states are automatically saved
+          </p>
+          <div className='flex gap-x-5 flex-shrink-0'>
+          <p className='text-text-secondary truncate text-sm min-w-0'>
+            Changes Saved
+          </p>
+          <p className='text-text-secondary text-sm truncate min-w-0'> 
+            Undo | Redo
+          </p>
+          </div>
+        </div>
+        <ActivateAutomationButton />
     </div>
   )
 }
